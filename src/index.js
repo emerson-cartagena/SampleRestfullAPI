a3a3c68048cf9726a074d3096983932b8cab20b3
Emerson Cartagena <emerson@techaffinity.com>
2024-04-08 07:52:59 -0600

    add index file

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
 
 const app = express();
 
+// Configurar Express para servir archivos estáticos
+app.use(express.static('public'));
+
 // Middleware
 app.use(bodyParser.json());
 
 // Rutas
 app.use('/api', librosRoutes);
+
+// Manejador de rutas para manejar rutas no definidas
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 // Puerto
 const PORT = process.env.PORT || 3000;
